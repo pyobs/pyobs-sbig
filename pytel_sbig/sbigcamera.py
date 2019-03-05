e85cdce8aa237b4812353d5ace64b2d239b26ea7
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-03-05 16:18:22 +0200

    using Exceptions for error propagation, and added docstrings

--- pytel_sbig/sbigcamera.py
+++ pytel_sbig/sbigcamera.py
@@ -3,7 +3,6 @@
 import logging
 import threading
 from datetime import datetime
-import numpy as np
 from astropy.io import fits
 
 from pytel.interfaces import ICamera, ICameraWindow, ICameraBinning, IFilters, ICooling
@@ -16,8 +15,17 @@ log = logging.getLogger(__name__)
 
 
 class SbigCamera(BaseCamera, ICamera, ICameraWindow, ICameraBinning, IFilters, ICooling):
+    """A pytel module for SBIG cameras."""
+
     def __init__(self, filter_wheel: str = 'UNKNOWN', filter_names: list = None, setpoint: float = -20,
                  *args, **kwargs):
+        """Initializes a new SbigCamera.
+
+        Args:
+            filter_wheel: Name of filter wheel used by the camera.
+            filter_names: List of filter names.
+            setpoint: Cooling temperature setpoint.
+        """
         BaseCamera.__init__(self, *args, **kwargs)
 
         # create cam
@@ -41,9 +49,13 @@ class SbigCamera(BaseCamera, ICamera, ICameraWindow, ICameraBinning, IFilters, I
         self._window = None
         self._binning = None
 
-    def open(self) -> bool:
-        if not BaseCamera.open(self):
-            return False
+    def open(self):
+        """Open module.
+
+        Raises:
+            ValueError: If cannot connect to camera or set filter wheel.
+        """
+        BaseCamera.open(self)
 
         # set filter wheel model
         if self._filter_wheel != FilterWheelModel.UNKNOWN:
@@ -51,16 +63,14 @@ class SbigCamera(BaseCamera, ICamera, ICameraWindow, ICameraBinning, IFilters, I
             try:
                 self._cam.set_filter_wheel(self._filter_wheel)
             except ValueError as e:
-                log.error('Could not set filter wheel: %s', e)
-                return False
+                raise ValueError('Could not set filter wheel: %s' % str(e))
 
         # open driver
         log.info('Opening SBIG driver...')
         try:
             self._cam.establish_link()
         except ValueError as e:
-            log.error('Could not establish link: %s', e)
-            return False
+            raise ValueError('Could not establish link: %s' % str(e))
 
         # get window and binning from camera
         self._window = self.get_full_frame()
@@ -70,29 +80,74 @@ class SbigCamera(BaseCamera, ICamera, ICameraWindow, ICameraBinning, IFilters, I
         self.set_cooling(self._setpoint is not None, self._setpoint)
 
     def close(self):
+        """Close module."""
         BaseCamera.close(self)
 
     def get_full_frame(self, *args, **kwargs) -> dict:
+        """Returns full size of CCD.
+
+        Returns:
+            Dictionary with left, top, width, and height set.
+
+        Raises:
+            ValueError: If full frame could not be obtained.
+        """
         width, height = self._cam.full_frame
         return {'left': 0, 'top': 0, 'width': width, 'height': height}
 
     def get_window(self, *args, **kwargs) -> dict:
+        """Returns the camera window.
+
+        Returns:
+            Dictionary with left, top, width, and height set.
+        """
         return self._window
 
     def get_binning(self, *args, **kwargs) -> dict:
+        """Returns the camera binning.
+
+        Returns:
+            Dictionary with x and y.
+        """
         return self._binning
 
-    def set_window(self, left: int, top: int, width: int, height: int, *args, **kwargs) -> bool:
+    def set_window(self, left: float, top: float, width: float, height: float, *args, **kwargs):
+        """Set the camera window.
+
+        Args:
+            left: X offset of window.
+            top: Y offset of window.
+            width: Width of window.
+            height: Height of window.
+        """
         self._window = {'left': int(left), 'top': int(top), 'width': int(width), 'height': int(height)}
         log.info('Setting window to %dx%d at %d,%d...', width, height, left, top)
-        return True
 
-    def set_binning(self, x: int, y: int, *args, **kwargs) -> bool:
+    def set_binning(self, x: int, y: int, *args, **kwargs):
+        """Set the camera binning.
+
+        Args:
+            x: X binning.
+            y: Y binning.
+        """
         self._binning = {'x': int(x), 'y': int(y)}
         log.info('Setting binning to %dx%d...', x, y)
-        return True
 
     def _expose(self, exposure_time: int, open_shutter: bool, abort_event: threading.Event) -> fits.PrimaryHDU:
+        """Actually do the exposure, should be implemented by derived classes.
+
+        Args:
+            exposure_time: The requested exposure time in ms.
+            open_shutter: Whether or not to open the shutter.
+            abort_event: Event that gets triggered when exposure should be aborted.
+
+        Returns:
+            The actual image.
+
+        Raises:
+            ValueError: If exposure was not successful.
+        """
+
         # set window/binning and exposure time
         self._cam.binning = self._binning
         self._cam.window = self._window
@@ -109,23 +164,15 @@ class SbigCamera(BaseCamera, ICamera, ICameraWindow, ICameraBinning, IFilters, I
         # init image
         self._img.image_can_close = False
 
-        # do exposure
-        try:
-            # start exposure
-            self._cam.expose(self._img, open_shutter)
-        except ValueError as e:
-            log.error('Could not take image: %s', e)
-            return None
+        # start exposure (can raise ValueError)
+        self._cam.expose(self._img, open_shutter)
 
         # wait for readout
         log.info('Exposure finished, reading out...')
         self._camera_status = ICamera.CameraStatus.READOUT
-        try:
-            # start readout
-            self._cam.readout(self._img, open_shutter)
-        except ValueError as e:
-            log.error('Could not read out image: %s', e)
-            return None
+
+        # start readout (can raise ValueError)
+        self._cam.readout(self._img, open_shutter)
 
         # finalize image
         self._img.image_can_close = True
@@ -168,7 +215,17 @@ class SbigCamera(BaseCamera, ICamera, ICameraWindow, ICameraBinning, IFilters, I
         self._camera_status = ICamera.CameraStatus.IDLE
         return hdu
 
-    def set_cooling(self, enabled: bool, setpoint: float, *args, **kwargs) -> bool:
+    def set_cooling(self, enabled: bool, setpoint: float, *args, **kwargs):
+        """Enables/disables cooling and sets setpoint.
+
+        Args:
+            enabled: Enable or disable cooling.
+            setpoint: Setpoint in celsius for the cooling.
+
+        Raises:
+            ValueError: If cooling could not be set.
+        """
+
         # log
         if enabled:
             log.info('Enabling cooling with a setpoint of %.2f°C...', setpoint)
@@ -176,16 +233,19 @@ class SbigCamera(BaseCamera, ICamera, ICameraWindow, ICameraBinning, IFilters, I
             log.info('Disabling cooling and setting setpoint to 20°C...')
 
         # do it
-        try:
-            self._cam.set_cooling(enabled, setpoint)
-        except ValueError as e:
-            log.error('Could not set cooling: %s', e)
-            return False
+        self._cam.set_cooling(enabled, setpoint)
 
-        # success
-        return True
+    def set_filter(self, filter_name: str, *args, **kwargs):
+        """Set the current filter.
 
-    def set_filter(self, filter_name: str, *args, **kwargs) -> bool:
+        Args:
+            filter_name: Name of filter to set.
+
+        Raises:
+            ValueError: If binning could not be set.
+            NotImplementedError: If camera doesn't have a filter wheel.
+        """
+
         # do we have a filter wheel?
         if self._filter_wheel == FilterWheelModel.UNKNOWN:
             raise NotImplementedError
@@ -196,30 +256,51 @@ class SbigCamera(BaseCamera, ICamera, ICameraWindow, ICameraBinning, IFilters, I
             raise ValueError('Unknown filter: %s', filter_name)
 
         # set it
-        try:
-            self._cam.set_filter(filters[filter_name])
-            return True
-        except ValueError as e:
-            log.error('Could not set filter: %s', e)
-            return False
+        self._cam.set_filter(filters[filter_name])
 
     def get_filter(self, *args, **kwargs) -> str:
+        """Get currently set filter.
+
+        Returns:
+            Name of currently set filter.
+
+        Raises:
+            ValueError: If filter could not be fetched.
+            NotImplementedError: If camera doesn't have a filter wheel.
+        """
+
         # do we have a filter wheel?
         if self._filter_wheel == FilterWheelModel.UNKNOWN:
             raise NotImplementedError
 
         # get current position and status
-        position, _ = self._cam.GetCFWPositionAndStatus()
+        position, _ = self._cam.get_filter_position_and_status()
         return self._filter_names[position]
 
     def list_filters(self, *args, **kwargs) -> list:
+        """List available filters.
+
+        Returns:
+            List of available filters.
+
+        Raises:
+            NotImplementedError: If camera doesn't have a filter wheel.
+        """
+
         # do we have a filter wheel?
         if self._filter_wheel == FilterWheelModel.UNKNOWN:
             raise NotImplementedError
 
+        # return names
         return [f for f in self._filter_names.values() if f is not None]
 
     def status(self, *args, **kwargs) -> dict:
+        """Returns current status of camera.
+
+        Raises:
+            ValueError: If status cannot be obtained.
+        """
+
         # get status from parent
         s = super().status()
 
