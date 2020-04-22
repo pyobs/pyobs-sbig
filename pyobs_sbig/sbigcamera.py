d57c24c2f752a8d84fab94cfbb2ea1daa080c609
Tim-Oliver Husser <thusser@uni-goettingen.de>
2020-04-22 15:51:45 +0200

    better dealing with blocked device

--- pyobs_sbig/sbigcamera.py
+++ pyobs_sbig/sbigcamera.py
@@ -38,6 +38,10 @@ class SbigCamera(BaseCamera, ICamera, ICameraWindow, ICameraBinning, ICooling):
         self._window = None
         self._binning = None
 
+        # cooling and temps to return in case of blocked device
+        self._cooling = None
+        self._temps = None
+
     def open(self):
         """Open module.
 
@@ -236,7 +240,13 @@ class SbigCamera(BaseCamera, ICamera, ICameraWindow, ICameraBinning, ICooling):
                 Power (float):          Current cooling power in percent or None.
         """
         enabled, temp, setpoint, _ = self._cam.get_cooling()
-        return enabled, temp, 0, {'CCD': temp}
+
+        # Nones?
+        if enabled is None and self._cooling is not None:
+            return self._cooling
+        else:
+            self._cooling = enabled, temp, 0, {'CCD': temp}
+            return self._cooling
 
     def get_temperatures(self, *args, **kwargs) -> dict:
         """Returns all temperatures measured by this module.
@@ -245,9 +255,13 @@ class SbigCamera(BaseCamera, ICamera, ICameraWindow, ICameraBinning, ICooling):
             Dict containing temperatures.
         """
         _, temp, _, _ = self._cam.get_cooling()
-        return {
-            'CCD': temp
-        }
+
+        # Nones?
+        if temp is None and self._temps is not None:
+            return self._temps
+        else:
+            self._temps = {'CCD': temp}
+            return self._temps
 
     def _abort_exposure(self):
         """Abort the running exposure. Should be implemented by derived class.
