15df132bdac6ca250caede6b5807fafc60f88613
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-03-12 17:28:48 +0100

    first version of a new sbig driver that is separated from the module to be used as shared module

---
+++ pyobs_sbig/sbigtracker.py
@@ -0,0 +1,23 @@
+import logging
+from typing import Union
+
+from .sbigbasecamera import SbigBaseCamera
+from .sbigudrv import *
+
+
+log = logging.getLogger(__name__)
+
+
+class SbigTracker(SbigBaseCamera):
+    """A pyobs module for the tracker chip in some SBIG cameras."""
+
+    def __init__(self, sensor: Union[str, ActiveSensor] = ActiveSensor.TRACKING, *args, **kwargs):
+        """Initializes a new SbigTracker.
+
+        Args:
+            setpoint: Cooling temperature setpoint.
+        """
+        SbigBaseCamera.__init__(self, sensor=sensor, *args, **kwargs)
+
+
+__all__ = ['SbigTracker']
