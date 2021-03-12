13f373c1da2468f46405880466beca2759ec2633
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-03-12 17:36:05 +0100

    added import

--- pyobs_sbig/__init__.py
+++ pyobs_sbig/__init__.py
@@ -1,3 +1,5 @@
+from .sbigdriver import SbigDriver
 from .sbigcamera import SbigCamera
+from .sbigtracker import SbigTracker
 from .sbigfiltercamera import SbigFilterCamera
 from .sbig6303e import Sbig6303eCamera
