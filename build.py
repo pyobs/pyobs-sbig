99bcc5b1ed348e472d403641b4be2b6e5648e283
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-11-19 14:00:16 +0100

    cython/poetry test

--- build.py
+++ build.py
@@ -1,36 +1,40 @@
-import os
+from setuptools import Extension, Distribution
+import numpy
+from Cython.Build import cythonize
+from Cython.Distutils.build_ext import new_build_ext as cython_build_ext
 
-# See if Cython is installed
-try:
-    from Cython.Build import cythonize
-# Do nothing if Cython is not available
-except ImportError:
-    # Got to provide this function. Otherwise, poetry will fail
-    def build(setup_kwargs):
-        pass
-# Cython is installed. Compile
-else:
-    from setuptools import Extension
-    from setuptools.dist import Distribution
-    from distutils.command.build_ext import build_ext
-    import numpy
 
-    # This function will be executed in setup.py:
-    def build(setup_kwargs):
-        # define extension
-        extension = Extension(
+def build():
+    extensions = [
+        Extension(
             'pyobs_sbig.sbigudrv',
             ['pyobs_sbig/sbigudrv.pyx', 'src/csbigcam.cpp', 'src/csbigimg.cpp'],
             libraries=['sbigudrv', 'cfitsio'],
             include_dirs=[numpy.get_include(), '/usr/include/cfitsio'],
             extra_compile_args=['-fPIC']
         )
+    ]
+    ext_modules = cythonize(
+        extensions,
+        compiler_directives={
+            "binding": True,
+            "language_level": "3"
+        },
+    )
 
-        # gcc arguments hack: enable optimizations
-        os.environ['CFLAGS'] = '-O3'
+    distribution = Distribution({
+        "name": "extended",
+        "ext_modules": ext_modules,
+        "cmdclass": {
+            "build_ext": cython_build_ext,
+        },
+    })
+    distribution.package_dir = "extended"
 
-        # Build
-        setup_kwargs.update({
-            'ext_modules': cythonize([extension]),
-            'cmdclass': {'build_ext': build_ext}
-        })
+    distribution.run_command("build_ext")
+    build_ext_cmd = distribution.get_command_obj("build_ext")
+    #build_ext_cmd.copy_extensions_to_source()
+
+
+if __name__ == "__main__":
+    build()
